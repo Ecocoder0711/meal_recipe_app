627112c0750717584f7f482b18f78dbf82aac74c
Ecocoder0711 <sakshamkashyapgame80@gmail.com>
2025-11-17 15:39:46 +0530

    first commit

---
+++ lib/runapp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:meal_recipe_app/screen/category_screen.dart';
+import 'package:meal_recipe_app/utils/theme_data.dart';
+
+class MealRecipeApp extends StatelessWidget {
+  const MealRecipeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Meal Recipe App",
+      home: CategoryScreen(),
+      theme: Mytheme.brightnesstheme(context),
+    );
+  }
+}
---
+++ lib/utils/theme_data.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class mytheme {
+  static ThemeData colorscheme(BuildContext context) => ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 180, 11, 57),);
+}
