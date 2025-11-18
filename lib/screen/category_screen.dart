1debf12ff836280401454fdc7dbcdc1f82ddd096
Ecocoder0711 <sakshamkashyapgame80@gmail.com>
2025-11-18 13:49:26 +0530

    gridView setup initiated

--- lib/screen/category_screen.dart
+++ lib/screen/category_screen.dart
@@ -5,6 +5,20 @@ class CategoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Category")),
+      body: GridView(
+        padding: EdgeInsets.all(8),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // har row kitne item
+          crossAxisSpacing: 20, // columns ke beech ka gap (horizontal)
+          mainAxisSpacing: 20, // rows ke beech ka gap (vertical)
+        ),
+        children: [
+          for (int i = 1; i < 20; i++)
+            Container(color: Colors.amber, child: Text("cell number $i")),
+        ],
+      ),
+    );
   }
 }
