a7a8a1bb1ac9e6e447e9cc087858e02b9e2be56f
Ecocoder0711 <sakshamkashyapgame80@gmail.com>
2025-11-18 13:15:40 +0530

    setup of Mytheme

--- lib/utils/theme_data.dart
+++ lib/utils/theme_data.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class mytheme {
-  static ThemeData colorscheme(BuildContext context) => ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 180, 11, 57),);
+class Mytheme {
+  static ThemeData brightnesstheme(BuildContext context) => ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color.fromARGB(255, 180, 11, 57),
+    ),
+    textTheme: GoogleFonts.latoTextTheme(),
+    scaffoldBackgroundColor: Colors.black,
+  );
 }
