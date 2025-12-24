312de3a86c0d58b83c302105ae619e3d9364153e
Ecocoder0711 <sakshamkashyapgame80@gmail.com>
2025-12-24 19:32:07 +0530

    meal recipe app work is finally completed.

--- lib/screen/recipe_page.dart
+++ lib/screen/recipe_page.dart
@@ -9,7 +9,62 @@ class RecipePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(meal.title)),
-      body: Center(child: Text("This the RecipePage")),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 14),
+            const Text(
+              "Ingredients",
+              style: TextStyle(
+                color: Colors.white70,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(height: 10),
+            // ... means we are using spread opearators
+            ...meal.ingredients.map((item) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 6,
+                ),
+                child: Text(item, style: TextStyle(color: Colors.white70)),
+              );
+            }),
+            const SizedBox(height: 10),
+            const Text(
+              "Steps",
+              style: TextStyle(
+                color: Colors.white70,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(height: 10),
+            ...meal.steps.map((step) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 6.0,
+                ),
+                child: Text(
+                  step,
+                  style: TextStyle(color: Colors.white70),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            }),
+          ],
+        ),
+      ),
     );
   }
 }
